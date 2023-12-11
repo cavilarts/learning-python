9778ae37c4c1a959fa9caf984db502dcc9755586
Carlos Avila <cavilart21@gmail.com>
2023-12-11 19:35:27 +0100

    initializing and learning to create a blackjack game

---
+++ project-black-jack/main.py
@@ -0,0 +1,85 @@
+import random
+
+class Card():
+  def __init__(self, suit, rank) -> None:
+    self.suit = suit
+    self.rank = rank
+    
+  def __str__(self) -> str:
+    return f"{self.rank["rank"]} of {self.suit}"
+
+class Deck:
+  def __init__(self) -> None:
+    self.cards = []
+    self.suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    self.ranks = [
+      { "rank": "A", "value": 11 },
+      {"rank": "2", "value": 2},
+      {"rank": "3", "value": 3},
+      {"rank": "4", "value": 4},
+      {"rank": "5", "value": 5},
+      {"rank": "6", "value": 6},
+      {"rank": "7", "value": 7},
+      {"rank": "8", "value": 8},
+      {"rank": "9", "value": 9},
+      {"rank": "10", "value": 10},
+      {"rank": "J", "value": 10},
+      {"rank": "Q", "value": 10},
+      {"rank": "K", "value": 10}
+    ]
+
+    for suit in self.suits:
+      for rank in self.ranks:
+        self.cards.append(Card(suit, rank))
+
+  def shuffle(self):
+    if len(self.cards) > 1:
+      random.shuffle(self.cards)
+    
+  def deal(self, n = 1):
+    cards_dealt = []
+    for x in range(n):
+      if len(self.cards) > 0:
+        cards_dealt.append(self.cards.pop())
+
+    return cards_dealt
+  
+class Hand:
+  def __init__(self, dealer=False) -> None:
+    self.cards = []
+    self.value = 0
+    self.dealer = dealer
+    
+  def add_card(self, card_list):
+      self.cards.extend(card_list)
+      
+  def calculate_value(self):
+    self.value = 0
+    has_ace = False
+    
+    for card in self.cards:
+      card_value = int(card.rank["value"])
+      self.value += card_value
+      if card.rank == "A":
+        has_ace = True
+        
+    if has_ace and self.value > 21:
+      self.value -= 10
+      
+  def get_value(self):
+    self.calculate_value()
+    return self.value
+  
+  def is_blackjack(self):
+    return self.get_value() == 21
+  
+  def display(self):
+    print(f"")
+
+      
+deck = Deck()
+deck.shuffle()
+hand = Hand()
+hand.add_card(deck.deal(2))
+
+print(hand.cards[0], hand.cards[1])
